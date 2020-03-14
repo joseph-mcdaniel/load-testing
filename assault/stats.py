db3fabdd2c474bce6687a918c1f576c38f569c4e
Joseph McDaniel <joewmcdaniel@gmail.com>
2020-03-14 13:11:35 +0000

    implement methods

--- assault/stats.py
+++ assault/stats.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from statistics import mean
 
 
 class Results:
@@ -18,7 +19,7 @@ class Results:
 
     def __init__(self, total_time: float, requests: List[Dict]):
         self.total_time = total_time
-        self.requests = requests
+        self.requests = sorted(requests, key=lambda r: r["request_time"])
 
     def slowest(self) -> float:
         """
@@ -37,7 +38,7 @@ class Results:
         >>> results.slowest()
         6.1
         """
-        pass
+        return self.requests[-1]["request_time"]
 
     def fastest(self) -> float:
         """
@@ -56,7 +57,7 @@ class Results:
         >>> results.fastest()
         1.04
         """
-        pass
+        return self.requests[0]["request_time"]
 
     def average_time(self) -> float:
         """
@@ -73,9 +74,9 @@ class Results:
         ...     'request_time': 1.04
         ... }])
         >>> results.average_time()
-        9.846666667
+        3.513333333333333
         """
-        pass
+        return mean([r["request_time"] for r in self.requests])
 
     def successful_requests(self) -> int:
         """
@@ -94,5 +95,5 @@ class Results:
         >>> results.successful_requests()
         2
         """
-        pass
+        return len([r for r in self.requests if r["status_code"] in range(200,299)])
 
